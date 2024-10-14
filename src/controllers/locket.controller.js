2c95ae841d6a68e3b9d356522ca1546afe4438cd
Kirra <74497428+PhuNguyn@users.noreply.github.com>
2024-10-14 07:20:49 +0700

    Update locket.controller.js

--- src/controllers/locket.controller.js
+++ src/controllers/locket.controller.js
@@ -13,7 +13,7 @@ class LocketController {
 
     async uploadMedia(req, res, next) {
         try {
-            const { userId, idToken, caption, captionColor, backgroundColor, textColor } = req.body;
+            const { userId, idToken, caption, captionColorTop, captionColorBottom } = req.body;
             const { images, videos } = req.files;
 
             if (!images && !videos) {
@@ -34,9 +34,8 @@ class LocketController {
                     idToken,
                     images[0],
                     caption,
-                    captionColor,
-                    backgroundColor,
-                    textColor
+                    captionColorTop,
+                    captionColorBottom
                 );
             } else {
                 if (videos[0].size > 10 * 1024 * 1024) {
@@ -50,9 +49,8 @@ class LocketController {
                     idToken,
                     videos[0],
                     caption,
-                    captionColor,
-                    backgroundColor,
-                    textColor
+                    captionColorTop,
+                    captionColorBottom
                 );
             }
 
