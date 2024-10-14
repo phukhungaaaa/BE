d131ae1e59a71e3c879adddaa8b97f606b9b1c11
Kirra <74497428+PhuNguyn@users.noreply.github.com>
2024-10-14 13:45:13 +0700

    Update locket.controller.js

--- src/controllers/locket.controller.js
+++ src/controllers/locket.controller.js
@@ -13,7 +13,7 @@ class LocketController {
 
     async uploadMedia(req, res, next) {
         try {
-            const { userId, idToken, caption, captionColorTop, captionColorBottom } = req.body;
+            const { userId, idToken, caption, topBgColor, bottomBgColor, textColor } = req.body;  // Nhận thêm màu từ request
             const { images, videos } = req.files;
 
             if (!images && !videos) {
@@ -29,13 +29,15 @@ class LocketController {
             }
 
             if (images) {
+                // Gửi màu sắc cùng với hình ảnh đến locketService
                 await locketService.postImage(
                     userId,
                     idToken,
                     images[0],
                     caption,
-                    captionColorTop,
-                    captionColorBottom
+                    topBgColor,      // Gửi thêm màu nền phần trên
+                    bottomBgColor,   // Gửi thêm màu nền phần dưới
+                    textColor        // Gửi thêm màu chữ
                 );
             } else {
                 if (videos[0].size > 10 * 1024 * 1024) {
@@ -44,13 +46,15 @@ class LocketController {
                     });
                 }
 
+                // Gửi màu sắc cùng với video đến locketService
                 await locketService.postVideo(
                     userId,
                     idToken,
                     videos[0],
                     caption,
-                    captionColorTop,
-                    captionColorBottom
+                    topBgColor,      // Gửi thêm màu nền phần trên
+                    bottomBgColor,   // Gửi thêm màu nền phần dưới
+                    textColor        // Gửi thêm màu chữ
                 );
             }
 
