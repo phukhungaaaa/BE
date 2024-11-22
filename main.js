5a618ccbbaa44bacec90bf33809be7c785d77adc
Kirra <74497428+PhuNguyn@users.noreply.github.com>
2024-11-23 03:58:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
-const Router = require("express");
-const router = Router();
-const locketController = require("../controllers/locket.controller.js");
-const passwordController = require("../controllers/password.controller.js");
-const friendsController = require("../controllers/friends.controller.js");
-const handleUpload = require("../middlewares/multipart-upload-support.middleware.js");
-const MAX_IMAGE_COUNT = 1;
-const MAX_VIDEO_COUNT = 1;
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const axios = require("axios");
+const envFile =
+    process.env.NODE_ENV === "production"
+        ? ".env.production"
+        : ".env.development";
 
-router.post("/login", locketController.login);
+dotenv.config({ path: envFile });
 
-router.post(
-    "/upload-media",
-    handleUpload(MAX_IMAGE_COUNT, MAX_VIDEO_COUNT),
-    locketController.uploadMedia
+const cors = require("cors");
+const { logInfo } = require("./src/services/logger.service.js");
+
+// Routers
+const routes = require("./src/routes");
+const errorHandler = require("./src/helpers/error-handler.js");
+
+const app = express();
+app.use(
+    cors({
+        origin: ["http://localhost:10000", "https://locket.pw"],
+        methods: ["GET", "POST"],
+
+        // Nhằm cho phép client gửi cookie lên server
+        credentials: true,
+    })
 );
 
-router.get("/ping", (req, res) => {
-    res.status(200).send("pong");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Nạp các route vào ứng dụng
+routes(app);
+
+// Middleware xử lý lỗi
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    logInfo("main.js", `Server backend is running at localhost:${PORT}`);
 });
 
-router.post("/forgot-password", passwordController.forgotPassword);
-
-router.post("/getFriends", friendsController.getFriends);
-
-module.exports = router;
+// Ping server mỗi 5 phút
+setInterval(() => {
+    axios.get(`https://phunguyn-locket-zmmw.onrender.com/locket/ping`, { timeout: 10000 }) // 10 giây timeout
+        .then(() => {
+            logInfo("Ping", `Pinged server at localhost:${PORT}/locket/ping`);
+        })
+        .catch((error) => {
+            console.error("Ping failed:", error.message);
+        });
+}, 300000); // 5 phút (300,000 ms)
