0a1169dd4012a33e1dc1be29898018eb921b3c55
Kirra <74497428+PhuNguyn@users.noreply.github.com>
2024-11-25 01:13:40 +0700

    Update locket.route.js

--- src/routes/locket.route.js
+++ src/routes/locket.route.js
@@ -3,6 +3,7 @@ const router = Router();
 const locketController = require("../controllers/locket.controller.js");
 const passwordController = require("../controllers/password.controller.js");
 const friendsController = require("../controllers/friends.controller.js");
+const profileController = require("../controllers/profile.controller.js");
 const handleUpload = require("../middlewares/multipart-upload-support.middleware.js");
 const MAX_IMAGE_COUNT = 1;
 const MAX_VIDEO_COUNT = 1;
@@ -22,5 +23,7 @@ router.get("/ping", (req, res) => {
 router.post("/forgot-password", passwordController.forgotPassword);
 
 router.post("/getFriends", friendsController.getFriends);
+
+router.post("/profile", profileController.getProfile);
 
 module.exports = router;
