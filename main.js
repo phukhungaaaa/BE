92b5a09bc133afefdcf8adaa61a96db7ab07ee19
Kirra <74497428+PhuNguyn@users.noreply.github.com>
2025-09-20 23:47:25 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const errorHandler = require("./src/helpers/error-handler.js");
 const app = express();
 app.use(
     cors({
-        origin: ["http://localhost:10000", "https://luckit-orpin.vercel.app"],
+        origin: ["http://localhost:10000", "https://locket.pw"],
         methods: ["GET", "POST"],
 
         // Nhằm cho phép client gửi cookie lên server
